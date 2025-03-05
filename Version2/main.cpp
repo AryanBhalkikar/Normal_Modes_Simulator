0b148dc5f23bbbffa896be28f91197b0dacb63d4
Aryan Bhalkikar <anirudh@M1.local>
2025-03-05 22:26:06 +0530

    Verion1 and Version2 complete

--- Version2/main.cpp
+++ Version2/main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include "gnuplot-iostream.h"
 
 #define PI 3.14159
 
-void amplitudeFunction(double w, std::vector<std::pair<int,double> > A0){
+void plotter(int N, int counter, double delay){
+    Gnuplot gp;
+    gp << "set term gif animate " << delay << " font 'Helvetica,16' enhanced\n";
+    gp << "set output 'oscillations.gif'\n";
+    gp << "set xrange [0:" << N+1 <<"]\n";
+    gp << "set yrange [-1.5:1.5]\n";
+    gp << "unset key\n";
+    gp << "set grid\n";
+    gp << "set xlabel 'Particle Number'\n";
+    gp << "set ylabel 'Amplitude'\n";
+    gp << "set xtics 0, 1," << N+1 << "\n";
+    gp << "do for [i = 0:" << counter <<"] {plot '/Users/anirudh/Documents/Physics Project Gnu/data.txt' index i w lines}\n";
+}
+
+void amplitudeFunction(double w, std::vector<std::pair<int,double> > A0, int N){
 
     std::ofstream op ("data.txt");
 
     double timePeriod = 2*PI/w;
+    int counter = 0;
+    double delay = 0.1;
     std::vector<std::pair<int, double> > A;
     
-    for (double t = 0; t < timePeriod; t = t+0.5){
+    for (double t = 0; t < 5*timePeriod; t = t+delay){
 
         A = A0;
 
@@ -24,9 +41,12 @@ void amplitudeFunction(double w, std::vector<std::pair<int,double> > A0){
             op << i.first << " " << i.second << std::endl;
         }
         op << std::endl << std::endl;
+        counter++;
     }
 
     op.close();
+
+    plotter(N, counter, delay);
 }
 
 void findMaxAmp(int n, int N, double w){
@@ -41,7 +61,7 @@ void findMaxAmp(int n, int N, double w){
         A0.push_back(std::make_pair(p, maxAmpOfP));
     }
     A0.push_back(std::make_pair(N+1, 0));
-    amplitudeFunction(w, A0);
+    amplitudeFunction(w, A0, N);
 }
 
 double naturalAngFreq(double T, double m, double l){
